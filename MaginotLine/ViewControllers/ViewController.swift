0e68c904da401b251eca53a2a153a5ac9532db61
jinunzzan <lovebox6607@gmail.com>
2022-11-22 19:38:28 +0900

    텍스트필드 연결 해제

--- MaginotLine/ViewControllers/ViewController.swift
+++ MaginotLine/ViewControllers/ViewController.swift
@@ -12,22 +12,14 @@ import Alamofire
 
 class ViewController: UIViewController, UITextFieldDelegate {
    
-    @IBOutlet weak var tfStart: UITextField!
-    var stations: Station?
     
-    @IBOutlet weak var tfEnd: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tfStart.delegate = self
-        tfEnd.delegate = self
-       
+
     }
-    
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        guard let vc = self.storyboard?.instantiateViewController(withIdentifier: "SelectStationViewController") as? SelectStationTableViewController else { return }
-          vc.beforeVC = self
-          self.present(vc, animated: true)
+
     }
   
     
@@ -35,4 +27,4 @@ class ViewController: UIViewController, UITextFieldDelegate {
 
     
     
-}
+
