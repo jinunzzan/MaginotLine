8b2ee1b8dbbf694aaed1c27446468c96cf742aaa
jinunzzan <lovebox6607@gmail.com>
2022-11-23 16:50:26 +0900

    출발역, 도착역, 도착시간 설정완료

--- MaginotLine/ViewControllers/ViewController.swift
+++ MaginotLine/ViewControllers/ViewController.swift
@@ -13,21 +13,23 @@ import Alamofire
 class ViewController: UIViewController{
     @IBOutlet weak var startBtn: UIButton!
     @IBOutlet weak var endBtn: UIButton!
+    var selectMaginotTime = ""
+    @IBOutlet weak var timeBtn: UIButton!
+    
     var stations: Station?
     
     let pickerListHour = ["03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26"]
     let pickerListMinute = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59"]
     let pickerListDate = ["평일", "토요일", "공휴일"]
-    let DateCode = ["1","2","3"] //WEEK_TAG
+    let dateCode = ["1","2","3"] //WEEK_TAG
     
     
-    var pickerView = UIPickerView()
+//    var pickerView = UIPickerView()
     var typeValue = String()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+      
     }
     // 출발역 도착역 선택 후 받아오기
     @IBAction func departureBtn(_ sender: Any){
@@ -55,8 +57,26 @@ class ViewController: UIViewController{
         pickerFrame.delegate = self
         pickerFrame.dataSource = self
         
+        
+        pickerFrame.selectRow(0, inComponent: 0, animated: false)
+        pickerFrame.selectRow(0, inComponent: 1, animated: false)
+        pickerFrame.selectRow(0, inComponent: 2, animated: false)
+        
         alert.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
-        alert.addAction(UIAlertAction(title: "확인", style: .default, handler: {(UIAlertAction) in print("\(self.typeValue)")
+//
+        // 확인 선택시 값 저장, 버튼에 출력
+        alert.addAction(UIAlertAction(title: "확인", style: .default, handler: { action in
+            let hour = self.pickerListHour[pickerFrame.selectedRow(inComponent: 0)]
+            let min = self.pickerListMinute[pickerFrame.selectedRow(inComponent: 1)]
+            let day = self.dateCode[pickerFrame.selectedRow(inComponent: 2)]
+            
+            self.selectMaginotTime = "\(hour):\(min)"
+            
+            self.timeBtn.setTitle(self.selectMaginotTime, for: .normal)
+            
+            
+            
+            print("\(hour)\(min)\(day)")
         }))
         self.present(alert, animated: true, completion: nil)
     }
